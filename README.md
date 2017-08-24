e98c7426ac4182bd394f2f97c4d01fbb8e6e663f
Tolsadus <maximeroux83320@gmail.com>
2017-08-24 13:53:28 +0200

    transfer from tolsadus

---
+++ README.md
@@ -0,0 +1,35 @@
+WTC Homebrew Fix
+===============
+
+This script installs Homebrew properly in your home on your 42 session.
+
+It fixes the `flock` issue due to NFS mount of your home directory.
+
+It will also move all temporary Homebrew data (`Temp`, `Cache`, `Locks`) to a dedicated folder in `/tmp`, leaving your home directory cleaner, so it's useful even if you're on the new ISCSI sessions that support `flock` operations. 
+
+
+Install
+-------
+Run this command from your terminal:
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)"
+```
+
+How it works
+------------
+This script removes your current Homebrew installation in your home, and reinstalls it properly from the Homebrew Github repo.
+
+Then it simply creates a `.brew_fix.zsh` script in your home directory, and modifies your `.zshrc` to source the script.
+
+It is simple to remove, and you can re-run it multiple times without duplication.
+
+
+Uninstall
+---------
+Simply remove these lines from your `.zshrc`
+```
+# Load Homebrew Fix script
+source $HOME/.brew_fix.zsh
+```
+
+And delete `.brew_fix.zsh` in your home directory.
---
+++ install.sh
@@ -0,0 +1,60 @@
+
+      #########.
+     ########",#:
+   #########',##".
+  ##'##'## .##',##.
+   ## ## ## # ##",#.
+    ## ## ## ## ##'
+     ## ## ## :##
+      ## ## ##."
+
+# Delete and reinstall Homebrew from Github repo
+rm -rf $HOME/.brew
+git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew
+
+# Create .brew_fix script in home directory
+cat > $HOME/.brew_fix.zsh <<EOL
+# HOMEBREW CONFIG
+
+# Add brew to path
+export PATH=\$HOME/.brew/bin:\$PATH
+
+# Set Homebrew temporary folders
+export HOMEBREW_CACHE=/tmp/\$USER/Homebrew/Caches
+export HOMEBREW_TEMP=/tmp/\$USER/Homebrew/Temp
+
+mkdir -p \$HOMEBREW_CACHE
+mkdir -p \$HOMEBREW_TEMP
+
+HOMEBREW_LOCKS_TARGET=/tmp/\$USER/Homebrew/Locks
+HOMEBREW_LOCKS_FOLDER=\$HOME/.brew/var/homebrew
+
+mkdir -p \$HOMEBREW_LOCKS_TARGET
+mkdir -p \$HOMEBREW_LOCKS_FOLDER
+
+# Symlink to Locks target folders
+# If not already a symlink
+if ! [[ -L \$HOMEBREW_LOCKS_FOLDER && -d \$HOMEBREW_LOCKS_FOLDER ]]; then
+  echo "Creating symlink for Locks folder"
+  rm -rf \$HOMEBREW_LOCKS_FOLDER
+  ln -s \$HOMEBREW_LOCKS_TARGET \$HOMEBREW_LOCKS_FOLDER
+fi
+
+EOL
+
+# Add .brew_fix sourcing in your .zshrc if not already present
+if ! grep -q "# Load Homebrew Fix script" "$HOME/.zshrc"; then
+
+cat >> $HOME/.zshrc <<EOL
+
+# Load Homebrew Fix script
+source \$HOME/.brew_fix.zsh
+EOL
+
+fi
+
+source $HOME/.brew_fix.zsh
+echo "brew update might take some time, please be patient"
+brew update
+
+echo "Please open a new terminal to apply modifications"
